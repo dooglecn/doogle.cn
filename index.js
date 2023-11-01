ab88499a9ef48708a75aafecd4d700c32d10d56c
errnull <errnull@libsvg.com>
2023-11-01 18:11:24 +0800

    feat: update icon.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-window.onload = function() {
-    document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+window.onload = function () {
+    var imageContainer = document.getElementById("main-content");
+    var imageUrl = "./IMG_1002.png";
+    var imageAlt = "doogle";
+
+    var imageHTML = `<img src="${imageUrl}" alt="${imageAlt}">`;
+
+    imageContainer.innerHTML = imageHTML;
 }
